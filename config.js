852d655b535b4bbaee56f87199f6bfc978dbf904
demon <demon@gmail.com>
2026-09-22 23:00:15 +0300

    Convert TEDDY-XMD source to BONY-XMD

---
+++ config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+
+global.APIs = {
+    xteam: 'https://api.xteam.xyz',
+    dzx: 'https://api.dhamzxploit.my.id',
+    lol: 'https://api.lolhuman.xyz',
+    violetics: 'https://violetics.pw',
+    neoxr: 'https://api.neoxr.my.id',
+    zenzapis: 'https://zenzapis.xyz',
+    akuari: 'https://api.akuari.my.id',
+    akuari2: 'https://apimu.my.id',
+    nrtm: 'https://fg-nrtm.ddns.net',
+    bg: 'http://bochil.ddns.net',
+    fgmods: 'https://api-fgmods.ddns.net'
+};
+
+
+
+module.exports = {
+    SESSION_ID: global.SESSION_ID || process.env.SESSION_ID,
+    WARN_COUNT: 3,
+    APIs: global.APIs,
+    APIKeys: {}
+};
---
+++ settings.js
@@ -0,0 +1,25 @@
+
+/**
+* supreme
+* caspar x
+*/
+
+const settings = {
+  packname: '✮⃝BONY-XMD',
+  author: '✮⃝BONY KE',
+  botName: "BONY-XMD",
+  botOwner: 'BONY KE', // Your name
+  ownerNumber: process.env.OWNER_NUMBER || '254748339103', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: '',
+  commandMode: "public",
+  maxStoreMessages: 20,
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.7.6",
+  updateBranch: 'main',
+  updateZipToken: '',
+  updateZipUrl: '',
+  timezone: 'Africa/Nairobi',
+};
+
+module.exports = settings;
